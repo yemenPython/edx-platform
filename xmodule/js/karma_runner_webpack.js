2107e388c09201ed594e1d3f49d0f572993b2aad
Syed Ali Abbas Zaidi <syed.ali.abbas@arbisoft.com>
2023-03-09 15:27:19 +0500

    fix: eslint space before function paren issue

--- xmodule/js/karma_runner_webpack.js
+++ xmodule/js/karma_runner_webpack.js
@@ -73,8 +73,8 @@ import './spec/time_spec.js';
 // Somehow the code initialized in jQuery's onready doesn't get called before karma auto starts
 
 'use strict';
-window.__karma__.loaded = function () {
-    setTimeout(function () {
+window.__karma__.loaded = function() {
+    setTimeout(function() {
         window.__karma__.start();
     }, 1000);
 };
